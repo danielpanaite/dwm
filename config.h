4574acf0fbbfe20508365d5d17d27c457e70a4ed
danielpanaite <dani.panaite@gmail.com>
2022-03-22 15:10:10 +0100

    tabs and border change

--- config.h
+++ config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
+static const unsigned int gappx     = 8;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cascadia Code:size=16", "CaskaydiaCove Nerd Font Mono:size=14" };
-static const char dmenufont[]       = "Cascadia Code:size=10";
+static const char *fonts[]          = { "Cascadia Code:size=16", "FontAwesome:size=14" };
+static const char dmenufont[]       = "Cascadia Code:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -23,7 +23,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
